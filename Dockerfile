746c433acb3bc71b9b94a62e8a3657e21cc09c7f
Ruben Hakopian <ruben.hakopian@gmail.com>
2018-07-02 19:02:57 -0700

    Adding zipkin tracing. In progress.

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM berliozcloud/golang-1.10
 
+RUN go get -u github.com/apache/thrift/lib/go/thrift
+
 ADD . /go/src/app
 RUN dep ensure
 
---
+++ zipkin.go
@@ -0,0 +1,11 @@
+package berlioz
+
+import (
+	// opentracing "github.com/opentracing/opentracing-go"
+	zipkin "github.com/openzipkin/zipkin-go-opentracing"
+)
+
+func testZipkin() {
+	zipkin.ClientServerSameSpan()
+	opentracing.kuku()
+}
