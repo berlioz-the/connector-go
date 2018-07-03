0ad3fa01a8c4a8a6b2c474838dfd217ded055d35
Ruben Hakopian <ruben.hakopian@gmail.com>
2018-07-03 14:50:56 -0700

    Got initial version of zipkin experiment working.

--- examples/hello/main.go
+++ examples/hello/main.go
@@ -2,46 +2,47 @@ package main
 
 import (
 	"log"
-	"time"
-
+	// "time"
 	// berlioz "connector-go.git"
 	"app"
-
-	"github.com/aws/aws-sdk-go/service/dynamodb"
+	// "github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
 func main() {
+	log.Printf("---------- PEER MONITOR -----------------")
 
-	berlioz.Peers("service", "app", "client").Monitor(func(peers berlioz.PeerAccessor) {
-		log.Printf("---------- PEER MONITOR -----------------")
-		log.Printf("--- PEERS: %v\n", peers.All())
-		if val, ok := peers.Get("1"); ok {
-			log.Printf("--- INDEXED PEER: %v\n", val)
-		}
-		if val, ok := peers.Random(); ok {
-			log.Printf("--- RANDOM PEER: %v\n", val)
-		}
-	})
+	berlioz.TestZipkin()
 
-	berlioz.Database("contacts").Monitor(func(database berlioz.NativeResourceAccessor) {
-		log.Printf("---------- DATABASE MONITOR -----------------")
+	// berlioz.Peers("service", "app", "client").Monitor(func(peers berlioz.PeerAccessor) {
+	// log.Printf("---------- PEER MONITOR -----------------")
+	// 	log.Printf("--- PEERS: %v\n", peers.All())
+	// 	if val, ok := peers.Get("1"); ok {
+	// 		log.Printf("--- INDEXED PEER: %v\n", val)
+	// 	}
+	// 	if val, ok := peers.Random(); ok {
+	// 		log.Printf("--- RANDOM PEER: %v\n", val)
+	// 	}
+	// })
 
-		params := &dynamodb.ScanInput{}
-		result, err := database.DynamoDB().Scan(params)
-		if err != nil {
-			log.Printf("--- DynamoDB::Scan Error: %v\n", err)
-		} else {
-			log.Printf("--- DynamoDB::Scan Result: %v\n", result)
-		}
-	})
+	// berlioz.Database("contacts").Monitor(func(database berlioz.NativeResourceAccessor) {
+	// 	log.Printf("---------- DATABASE MONITOR -----------------")
 
-	resp, body, err := berlioz.Request("service", "app", "client").Get("/")
-	if err != nil {
-		log.Printf("--- Response Error: %s\n", err)
-	} else {
-		log.Printf("--- Response Status Code: %s\n", resp.Status)
-		log.Printf("--- Response Body: %s\n", body)
-	}
+	// 	params := &dynamodb.ScanInput{}
+	// 	result, err := database.DynamoDB().Scan(params)
+	// 	if err != nil {
+	// 		log.Printf("--- DynamoDB::Scan Error: %v\n", err)
+	// 	} else {
+	// 		log.Printf("--- DynamoDB::Scan Result: %v\n", result)
+	// 	}
+	// })
 
-	time.Sleep(5 * time.Second)
+	// resp, body, err := berlioz.Request("service", "app", "client").Get("/")
+	// if err != nil {
+	// 	log.Printf("--- Response Error: %s\n", err)
+	// } else {
+	// 	log.Printf("--- Response Status Code: %s\n", resp.Status)
+	// 	log.Printf("--- Response Body: %s\n", body)
+	// }
+
+	// time.Sleep(5 * time.Second)
 }
--- zipkin.go
+++ zipkin.go
@@ -3,29 +3,68 @@ package berlioz
 // opentracing "github.com/opentracing/opentracing-go"
 // zipkin "github.com/openzipkin/zipkin-go-opentracing"
 import (
-	_ "github.com/opentracing/opentracing-go"
+	"context"
+	"log"
+	"time"
+
+	"fmt"
+	"os"
+
+	opentracing "github.com/opentracing/opentracing-go"
+
+	zipkin "github.com/openzipkin/zipkin-go-opentracing"
 	// _ "github.com/apache/thrift"
 )
 
-func testZipkin() {
-	// collector, err := zipkin.NewHTTPCollector("http://localhost:9411/api/v1/spans")
-	// if err != nil {
-	// 	fmt.Printf("unable to create Zipkin HTTP collector: %+v\n", err)
-	// 	os.Exit(-1)
-	// }
+func TestZipkin() {
 
-	// // create recorder.
-	// recorder := zipkin.NewRecorder(collector, false, "127.0.0.1:61001", "serviceName")
+	time.Sleep(1 * time.Second)
 
-	// // create tracer.
-	// ///tracer
-	// trader, err := zipkin.NewTracer(
-	// 	recorder,
-	// 	zipkin.ClientServerSameSpan(true),
-	// 	zipkin.TraceID128Bit(true),
-	// )
-	// if err != nil {
-	// 	fmt.Printf("unable to create Zipkin tracer: %+v\n", err)
-	// 	os.Exit(-1)
-	// }
+	log.Printf("[TestZipkin] init \n")
+
+	logger1 := log.New(os.Stdout, log.Prefix(), log.Flags())
+	logger := zipkin.LogWrapper(logger1)
+	logger.Log("AAAAAAAAAA")
+	logger1.Println("BBBBBBBBB")
+
+	collector, err := zipkin.NewHTTPCollector("http://172.17.0.2:9411/api/v1/spans", zipkin.HTTPLogger(logger))
+	if err != nil {
+		fmt.Printf("unable to create Zipkin HTTP collector: %+v\n", err)
+		os.Exit(-1)
+	}
+	log.Printf("[TestZipkin] collector created \n")
+
+	// create recorder.
+	recorder := zipkin.NewRecorder(collector, true, "127.0.0.1:61001", "serviceName")
+	log.Printf("[TestZipkin] recorder created \n")
+
+	// create tracer.
+	tracer, err := zipkin.NewTracer(
+		recorder,
+		zipkin.ClientServerSameSpan(true),
+		zipkin.TraceID128Bit(true),
+		zipkin.WithLogger(logger),
+	)
+	if err != nil {
+		fmt.Printf("unable to create Zipkin tracer: %+v\n", err)
+		os.Exit(-1)
+	}
+
+	log.Printf("[TestZipkin] tracer created \n")
+
+	span := tracer.StartSpan("MyOp1")
+	log.Printf("[TestZipkin] span started \n")
+	opentracing.ContextWithSpan(context.Background(), span)
+
+	time.Sleep(500 * time.Millisecond)
+	span.Finish()
+
+	log.Printf("[TestZipkin] end \n")
+
+	// tracer
+	time.Sleep(500 * time.Millisecond)
+
+	// collector.Collect(&span)
+	collector.Close()
+
 }
