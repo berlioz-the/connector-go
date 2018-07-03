c098f9145b0f41a25e1c95b04a2ddc623e1870c7
Ruben Hakopian <ruben.hakopian@gmail.com>
2018-07-03 16:49:07 -0700

    moved samples into separate repo

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM berliozcloud/golang-1.10
-
-# RUN go get -u github.com/apache/thrift/lib/go/thrift
-
-ADD . /go/src/app
-RUN dep ensure
-
-RUN go build -o /app/main examples/hello/*
--- test-run.sh
+++
@@ -1,7 +0,0 @@
-berlioz local push-run --quick --cluster berliozgo --service example
-echo '==============================================================================='
-echo '==============================================================================='
-echo '==============================================================================='
-read -p "Pausing to fetch logs..." -t 2
-echo ""
-docker ps -a | grep berliozgo-example | head -n 1 | awk '{print $1}' | xargs docker logs
